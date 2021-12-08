52fc18922f405f4007017e89a2fca3296b3a9c5e
WooooCash <lukaszbialczak3@gmail.com>
2021-12-08 19:55:16 +0100

    displaying different dashboard based on status

--- src/main/java/com/example/CashRegister/MainFrame.java
+++ src/main/java/com/example/CashRegister/MainFrame.java
@@ -60,11 +60,11 @@ public class MainFrame extends JFrame {
 
 
         //Add content based on some sort of int/enum/bool depending on role
-        if (status == 0) {} // create new panel for regular employee
-        else if (status == 1) {} // create new panel for manager
+        if (status == 0) {centerPanel = new EmployeeDashboardPanel();} // create new panel for regular employee
+        else if (status == 1) {centerPanel = new ManagerDashboardPanel();} // create new panel for manager
 //        centerPanel = new JPanel(); //switch out for custom panel class? based on user priviledges
 //        centerPanel.setBackground(Color.blue);
-        centerPanel = new EmployeeDashboardPanel();
+
 
         frame.add(centerPanel, BorderLayout.CENTER);
 
